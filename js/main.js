5c359dbbbb0bf3e3ed15bb12335f5689fd2ebe64
touros91 <andreageraci91@gmail.com>
2021-09-20 14:37:21 +0200

    Refactoring

--- js/main.js
+++ js/main.js
@@ -4,14 +4,29 @@ const app = new Vue(
         data: {
             discoverSicilyImage: "img/discover-sicily.png",
             altTextDiscoverSicily: "Discover Sicily image",
-            images: ["https://images.musement.com/cover/0002/52/cathedral-of-palermo-xl-jpg_header-151876.jpeg",
-            "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/catania-cosa-vedere-1619507580.jpg",
-            "https://www.bravodriver.com/assets/img/italy/sicily/ragusa/ragusa_ibla.jpg",
-            "https://a.cdn-hotels.com/gdcs/production178/d1521/a0d859a0-5863-11e8-a5ee-0242ac110030.jpg",
-            "https://media-cdn.tripadvisor.com/media/vr-splice-j/00/7d/e1/99.jpg",
-            "https://www.eleonoraongaro.it/wp-content/uploads/2021/05/Biglietti-valle-dei-templi-Agrigento-1030x687.jpg",
-            "https://www.labisialacarte.it/wp-content/uploads/2020/07/Cefal%C3%B9-primavera.jpg"],
-            altTextImages: ["Palermo", "Catania", "Ragusa", "Taormina", "San Vito Lo Capo", "Valle dei Templi", "Cefalù"],
+            images: [
+                { src:"https://images.musement.com/cover/0002/52/cathedral-of-palermo-xl-jpg_header-151876.jpeg",
+                  altText: "Palermo"
+                },
+                { src: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/catania-cosa-vedere-1619507580.jpg",
+                  altText: "Catania"
+                },
+                { src: "https://www.bravodriver.com/assets/img/italy/sicily/ragusa/ragusa_ibla.jpg",
+                  altText: "Ragusa"
+                },
+                { src: "https://a.cdn-hotels.com/gdcs/production178/d1521/a0d859a0-5863-11e8-a5ee-0242ac110030.jpg",
+                  altText: "Taormina"
+                },
+                { src:"https://media-cdn.tripadvisor.com/media/vr-splice-j/00/7d/e1/99.jpg",
+                  altText: "San Vito Lo Capo"
+                },
+                { src: "https://www.eleonoraongaro.it/wp-content/uploads/2021/05/Biglietti-valle-dei-templi-Agrigento-1030x687.jpg",
+                  altText: "Valle dei Templi"
+                },
+                { src: "https://www.labisialacarte.it/wp-content/uploads/2020/07/Cefal%C3%B9-primavera.jpg",
+                  altText: "Cefalù"
+                }
+            ],
             imageCounter: 0,
             autoPlay: null
         },
